ca51fb6e540a1fe204db9ad954966364cea55eda
Imran Anwar <imranwar@gmail.com>
2014-06-03 16:59:10 +0500

    First Version

---
+++ Banckle-Helpdesk-for-Joomla-2.5.x/mod_bancklehelpdesk/mod_bancklehelpdesk.php
@@ -0,0 +1,13 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_bancklehelpdesk - Banckle Helpdesk for Joomla
+# ------------------------------------------------------------------------
+# author    Masood Anwer <masood.anwer@banckle.com>
+# copyright Copyright (C) 2014 banckle.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://banckle.com
+-------------------------------------------------------------------------*/
+// no direct access
+defined( "_JEXEC" ) or die;
+
+require(JModuleHelper::getLayoutPath('mod_bancklehelpdesk'));
---
+++ Banckle-Helpdesk-for-Joomla-3.x/tmpl/default.php
@@ -0,0 +1,17 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_bancklehelpdesk - Banckle Helpdesk for Joomla
+# ------------------------------------------------------------------------
+# author    Masood Anwer <masood.anwer@banckle.com>
+# copyright Copyright (C) 2014 banckle.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://banckle.com
+-------------------------------------------------------------------------*/
+// no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<div id="banckleHelpdeskWidget">
+<?php
+    echo $params->get("banckleHelpdeskWidget");
+?>
+</div>
